0770627fc231222a4e735f875790a67ed3b5eecf
Ryan Schlemer <rschlem1@gmail.com>
2021-02-07 17:11:53 -0600

    missing component

---
+++ components/meta.js
@@ -0,0 +1,37 @@
+import { Component } from 'react';
+
+class Meta extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const SEO = {
+            title: 'Melvor Mastery Calculator',
+            description: "Calculate time and material requirements to max a skill's mastery in Melvor Idle.",
+            image: "https://raw.githubusercontent.com/rschlemer/mastery-calculator/main/public/index.png"
+        };
+        console.log(SEO.image)
+        return (
+            <>
+                <meta name="description" content={SEO.description} />
+
+                {/* Open Graph */}
+                <meta property="og:title" content={SEO.title} />
+                <meta property="og:description" content={SEO.description} />
+                <meta property="og:image" content={SEO.image.props} />
+                <meta property="og:url" content="/" />
+
+                {/* Twitter */}
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta property="twitter:domain" content="/" />
+                <meta property="twitter:url" content="/" />
+                <meta name="twitter:title" content={SEO.title} />
+                <meta name="twitter:description" content={SEO.description} />
+                <meta name="twitter:image" content={SEO.image} />
+            </>
+        )
+    }
+}
+
+export { Meta }
