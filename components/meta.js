2f5c48fb325d2478cb91fe894ecb8f9eb617cee6
Ryan Schlemer <rschlem1@gmail.com>
2021-02-13 18:00:13 -0600

    twitter

--- components/meta.js
+++ components/meta.js
@@ -9,7 +9,8 @@ class Meta extends Component {
         const SEO = {
             title: 'Melvor Mastery Calculator',
             description: "Calculate time and material requirements to max a skill's mastery in Melvor Idle.",
-            image: "https://raw.githubusercontent.com/rschlemer/mastery-calculator/main/public/index.png"
+            image: "https://raw.githubusercontent.com/rschlemer/mastery-calculator/main/public/index.png",
+            url: "https://mastery-calculator.vercel.app/"
         };
         console.log(SEO.image)
         return (
@@ -20,12 +21,11 @@ class Meta extends Component {
                 <meta property="og:title" content={SEO.title} />
                 <meta property="og:description" content={SEO.description} />
                 <meta property="og:image" content={SEO.image} />
-                <meta property="og:url" content="/" />
+                <meta property="og:url" content={SEO.url} />
 
                 {/* Twitter */}
                 <meta name="twitter:card" content="summary_large_image" />
-                <meta property="twitter:domain" content="/" />
-                <meta property="twitter:url" content="/" />
+                <meta property="twitter:url" content={SEO.url} />
                 <meta name="twitter:title" content={SEO.title} />
                 <meta name="twitter:description" content={SEO.description} />
                 <meta name="twitter:image" content={SEO.image} />
